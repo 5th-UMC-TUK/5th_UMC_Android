edd44a0a76f9019efe5248d13fc344ef695fd9b9
KKJ <kyujin0911@tukorea.ac.kr>
2023-09-24 20:58:28 +0900

    1주차 미션

---
+++ 플램/FlonClone/app/src/main/java/umc/mission/floclone/NewMusicDaily.kt
@@ -0,0 +1,10 @@
+package umc.mission.floclone
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+
+data class NewMusicDaily(
+    val title: String? = null,
+    val singer: String? = null,
+    val musicImage: Drawable? = null
+)
